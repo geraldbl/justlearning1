2746464e6a0939705db124cb0e50796c97eb1192
Gerald <gerald-b@runbox.com>
2021-05-03 08:55:38 +0800

    Initial version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.WriteLine("You wrote your first lines of code!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
